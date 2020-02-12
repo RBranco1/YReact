eee6b8c0230e36b1d7450b630bf19c42556f36ca
Raphael <raphael.pieroni@aluno.soueinstein.com.br>
2020-02-12 14:46:27 -0300

    resolver bug do grid,, esta para cima de mais

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import logo from './images/flower.png';
+import avatar from './images/avatar.png';
 
 function App() {
   return (
@@ -8,17 +9,16 @@ function App() {
       <div className="App">
         <div class="ui menu inverted fixed">
           <img src={logo} />
-          <a class="item">algo</a>
+          <a class="item"> Algo</a>
           <button class="ui teal inverted button">Postar Projeto</button>
           <div class="ui icon input">
             <input type="text" placeholder="Buscar..." />
             <i aria-hidden="true" class="search icon"></i>
           </div>
         </div>
-      </div>
-      <div class="ui container three column grid">
-          <div class="ui card">
-            <div class="image"><img src="/images/avatar/large/matthew.png" /></div>
+        <div class="ui container three column grid">
+          <div class="column"><div class="ui card">
+            <div class="image"><img src={avatar} /></div>
             <div class="content">
               <div class="header">Matthew</div>
               <div class="meta"><span class="date">Joined in 2015</span></div>
@@ -28,12 +28,41 @@ function App() {
               <a>
                 <i aria-hidden="true" class="user icon"></i>
                 22 Friends
-              </a>
+    </a>
             </div>
-          </div>
-          <div class="column"><img src="/images/wireframe/image.png" class="ui image" /></div>
-          <div class="column"><img src="/images/wireframe/image.png" class="ui image" /></div>
+          </div></div>
+          <div class="column"><div class="ui card">
+            <div class="image"><img src={avatar} /></div>
+            <div class="content">
+              <div class="header">Matthew</div>
+              <div class="meta"><span class="date">Joined in 2015</span></div>
+              <div class="description">Matthew is a musician living in Nashville.</div>
+            </div>
+            <div class="extra content">
+              <a>
+                <i aria-hidden="true" class="user icon"></i>
+                22 Friends
+    </a>
+            </div>
+          </div></div>
+          <div class="column"><div class="ui card">
+            <div class="image"><img src={avatar} /></div>
+            <div class="content">
+              <div class="header">Matthew</div>
+              <div class="meta"><span class="date">Joined in 2015</span></div>
+              <div class="description">Matthew is a musician living in Nashville.</div>
+            </div>
+            <div class="extra content">
+              <a>
+                <i aria-hidden="true" class="user icon"></i>
+                22 Friends
+    </a>
+            </div>
+          </div></div>
         </div>
+      </div>
+
+
     </>
   );
 }
