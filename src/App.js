f653bc2ca82db8a21aebaf40f1b27ed8c839f735
pedro <pedro>
2020-02-19 22:20:03 -0300

    aula 4 concluida

--- src/App.js
+++ src/App.js
@@ -1,70 +1,82 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import logo from './images/flower.png';
 import avatar from './images/avatar.png';
 
-function App() {
-  return (
-    <>
-      <div className="App">
-        <div class="ui menu inverted fixed">
-          <img src={logo} />
-          <a class="item"> Algo</a>
-          <button class="ui teal inverted button">Postar Projeto</button>
-          <div class="ui icon input">
-            <input type="text" placeholder="Buscar..." />
-            <i aria-hidden="true" class="search icon"></i>
+
+
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      lista: [
+        {
+          nome: "Nome aleatorio",
+          descricao: "descrição aleatoria",
+          usuario: "Autor Aleatorio",
+        }
+      ]
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8080/api/projetos")
+    .then(res => res.json())
+    .then((result) => {
+      this.setState({lista: result.data})
+    })
+  }
+
+  render() {
+    return (
+      <>
+      
+
+        <div className="App">
+          <div class="ui menu inverted fixed">
+            <img src={logo} />
+            <a class="item"> Algo</a>
+            <button class="ui teal inverted button">Postar Projeto</button>
+            <div class="ui icon input">
+              <input type="text" placeholder="Buscar..." />
+              <i aria-hidden="true" class="search icon"></i>
+            </div>
           </div>
+          <div id="space">
+          <div class="ui container three column grid">
+          {this.state.lista.map(function(projeto) {
+              return (
+                <div class="column">
+                  <div class="ui card">
+                    <div class="image">
+                      <img src={avatar} />
+                    </div>
+                    <div class="content">
+                      <div class="header"> {projeto.nome} </div>
+                      <div class="meta">
+                        <span class="date">{projeto.usuario}</span>
+                      </div>
+                      <div class="description">
+                        {projeto.descricao}
+                      </div>
+                    </div>
+                    <div class="extra content">
+                      <a>
+                        <i aria-hidden="true" class="like icon"></i>
+                        {projeto.likes} Likes
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
         </div>
-        <div class="ui container three column grid">
-          <div class="column"><div class="ui card">
-            <div class="image"><img src={avatar} /></div>
-            <div class="content">
-              <div class="header">Matthew</div>
-              <div class="meta"><span class="date">Joined in 2015</span></div>
-              <div class="description">Matthew is a musician living in Nashville.</div>
-            </div>
-            <div class="extra content">
-              <a>
-                <i aria-hidden="true" class="user icon"></i>
-                22 Friends
-    </a>
-            </div>
-          </div></div>
-          <div class="column"><div class="ui card">
-            <div class="image"><img src={avatar} /></div>
-            <div class="content">
-              <div class="header">Matthew</div>
-              <div class="meta"><span class="date">Joined in 2015</span></div>
-              <div class="description">Matthew is a musician living in Nashville.</div>
-            </div>
-            <div class="extra content">
-              <a>
-                <i aria-hidden="true" class="user icon"></i>
-                22 Friends
-    </a>
-            </div>
-          </div></div>
-          <div class="column"><div class="ui card">
-            <div class="image"><img src={avatar} /></div>
-            <div class="content">
-              <div class="header">Matthew</div>
-              <div class="meta"><span class="date">Joined in 2015</span></div>
-              <div class="description">Matthew is a musician living in Nashville.</div>
-            </div>
-            <div class="extra content">
-              <a>
-                <i aria-hidden="true" class="user icon"></i>
-                22 Friends
-    </a>
-            </div>
-          </div></div>
         </div>
-      </div>
-
-
-    </>
-  );
-}
+        </div>
+      </>
+    );
+            }
+          }
 
 export default App;
